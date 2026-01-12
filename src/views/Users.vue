532a8fb0e5fa6e17e51c871e5856e92dcd7fc62b
schammahmuhizi <38748323+schammahmuhizi@users.noreply.github.com>
2026-01-12 15:42:31 +0200

    Add users

--- src/views/Users.vue
+++ src/views/Users.vue
@@ -1,8 +1,44 @@
 <template>
   <div class="container-fluid p-0">
-    <h1 class="h3 mb-4">Gestion des Utilisateurs</h1>
-    <div class="card glass text-center p-5 text-muted">
-      Gérez les utilisateurs du système et les permissions ici.
+    <div class="d-flex justify-content-between">
+    <h1 class="h5 mb-4">Gestion des Utilisateurs</h1>
+      <RouterLink to="/users/create" class="btn btn-primary mt-3">Ajouter un utilisateur</RouterLink>
+    </div>
+
+    <div class="card glass text-center text-muted">
+      <table class="table table-hover">
+        <thead>
+          <tr>
+            <th scope="col">#</th>
+            <th scope="col">Nom</th>
+            <th scope="col">Prenom</th>
+            <th scope="col">Email</th>
+            <th scope="col">Role</th>
+            <th scope="col">Actions</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr v-for="user in users" :key="user.id">
+            <th scope="row">1</th>
+            <td>{{ user.name }}</td>
+            <td>{{ user.prenom }}</td>
+            <td>{{ user.email }}</td>
+            <td>{{ user.role }}</td>
+            <td>
+              <RouterLink to="/users/edit" class="btn btn-primary">Edit</RouterLink>
+            </td>
+          </tr>
+        </tbody>
+      </table>
     </div>
   </div>
 </template>
+
+<script setup>
+import { ref } from 'vue';
+
+const users = ref([
+  { id: 1, name: 'John', prenom: 'Doe', email: 'john@example.com', role: 'admin' },
+  { id: 2, name: 'Jane', prenom: 'Doe', email: 'jane@example.com', role: 'user' },
+]);
+</script>
